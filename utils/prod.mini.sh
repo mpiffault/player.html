e4211f8408cbc0a827642b5c00d8f6fcd0ed6d2f
paquito <rms@gnu.org>
2015-10-11 13:47:24 +0200

    removed newlines and <body></body> :D

--- utils/prod.mini.sh
+++ utils/prod.mini.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo '<!DOCTYPE HTML><html><head><meta charset="UTF-8" /><script>'
+echo -n '<!DOCTYPE HTML><html><head><meta charset="UTF-8" /><script>'
 java -jar closure/compiler.jar --js script.js
-echo '</script><style>'
+echo -n '</script><style>'
 tr -d "\n" < style.css | sed -e 's/  */ /g' -e 's/\([;:{]\) /\1/g' -e 's/;}/}\n/g' -e 's/ {/{/g'
-echo '</style></head><body></body></html>'
+echo -n '</style></head></html>'
